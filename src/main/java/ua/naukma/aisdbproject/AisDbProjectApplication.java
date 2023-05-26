85fbeb5cab8867116aa98da33f99a280ed0b63e5
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-05-26 19:19:36 +0300

    Initial commit for back-end

---
+++ src/main/java/ua/naukma/aisdbproject/AisDbProjectApplication.java
@@ -0,0 +1,13 @@
+package ua.naukma.aisdbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisDbProjectApplication.class, args);
+	}
+
+}
