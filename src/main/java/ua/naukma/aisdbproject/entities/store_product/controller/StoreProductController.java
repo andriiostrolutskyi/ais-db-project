06edc809395e602178633946dde9f4817fc1de41
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-06-02 22:42:28 +0300

    Add StoreProduct controller, dao, model

---
+++ src/main/java/ua/naukma/aisdbproject/entities/store_product/controller/StoreProductController.java
@@ -0,0 +1,71 @@
+package ua.naukma.aisdbproject.entities.store_product.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ua.naukma.aisdbproject.entities.store_product.dao.StoreProductDAO;
+import ua.naukma.aisdbproject.entities.store_product.model.StoreProduct;
+
+@Controller
+@RequestMapping("/api/v1/store-product")
+public class StoreProductController {
+    private final StoreProductDAO storeProductDAO;
+
+    @Autowired
+    public StoreProductController(StoreProductDAO storeProductDAO) {
+        this.storeProductDAO = storeProductDAO;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("categories", storeProductDAO.getAll());
+        return "storeProduct/show";
+    }
+
+    @GetMapping("/{upc}")
+    public String getByID(@PathVariable("upc") String upc, Model model) {
+        model.addAttribute("storeProduct", storeProductDAO.getByID(upc));
+        return "storeProduct/show";
+    }
+
+    @GetMapping("/add-store-product")
+    public String goToAdd(Model model) {
+        model.addAttribute("storeProduct", new StoreProduct());
+        return "storeProduct/add";
+    }
+
+    @PostMapping
+    public String add(@ModelAttribute("storeProduct") @Valid StoreProduct storeProduct,
+                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "storeProduct/add";
+        storeProductDAO.add(storeProduct);
+        return "redirect:/api/v1/store-product";
+    }
+
+    @GetMapping("/{upc}/edit")
+    public String edit(Model model,
+                       @PathVariable("upc") String upc) {
+        model.addAttribute("storeProduct", storeProductDAO.getByID(upc));
+        return "storeProduct/edit";
+    }
+
+    @PatchMapping("/{upc}")
+    public String update(@ModelAttribute("storeProduct") @Valid StoreProduct storeProduct,
+                         BindingResult bindingResult,
+                         @PathVariable("upc") String upc) {
+        if (bindingResult.hasErrors())
+            return "storeProduct/edit";
+        storeProductDAO.update(upc, storeProduct);
+        return "redirect:/api/v1/store-product";
+    }
+
+    @DeleteMapping("/{upc}")
+    public String delete(@PathVariable("upc") String upc) {
+        storeProductDAO.delete(upc);
+        return "redirect:/api/v1/store-product";
+    }
+}
---
+++ src/main/java/ua/naukma/aisdbproject/entities/store_product/model/StoreProduct.java
@@ -0,0 +1,90 @@
+package ua.naukma.aisdbproject.entities.store_product.model;
+
+public class StoreProduct {
+    private String upc;
+    private String upcProm;
+    private Integer idProduct;
+    private Float sellingPrice;
+    private Integer productsNumber;
+    private Boolean promotionalProduct;
+
+    public StoreProduct() {
+    }
+
+    public StoreProduct(String upc, Integer idProduct, Float sellingPrice, Integer productsNumber, Boolean promotionalProduct) {
+        this.upc = upc;
+        this.idProduct = idProduct;
+        this.sellingPrice = sellingPrice;
+        this.productsNumber = productsNumber;
+        this.promotionalProduct = promotionalProduct;
+    }
+
+    public StoreProduct(String upc, String upcProm, Integer idProduct, Float sellingPrice, Integer productsNumber, Boolean promotionalProduct) {
+        this.upc = upc;
+        this.upcProm = upcProm;
+        this.idProduct = idProduct;
+        this.sellingPrice = sellingPrice;
+        this.productsNumber = productsNumber;
+        this.promotionalProduct = promotionalProduct;
+    }
+
+    @Override
+    public String toString() {
+        return "StoreProduct{" +
+                "upc='" + upc + '\'' +
+                ", upcProm='" + upcProm + '\'' +
+                ", idProduct=" + idProduct +
+                ", sellingPrice=" + sellingPrice +
+                ", productsNumber=" + productsNumber +
+                ", promotionalProduct=" + promotionalProduct +
+                '}';
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+
+    public String getUpcProm() {
+        return upcProm;
+    }
+
+    public void setUpcProm(String upcProm) {
+        this.upcProm = upcProm;
+    }
+
+    public Integer getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Integer idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public Float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Float sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Integer getProductsNumber() {
+        return productsNumber;
+    }
+
+    public void setProductsNumber(Integer productsNumber) {
+        this.productsNumber = productsNumber;
+    }
+
+    public Boolean getPromotionalProduct() {
+        return promotionalProduct;
+    }
+
+    public void setPromotionalProduct(Boolean promotionalProduct) {
+        this.promotionalProduct = promotionalProduct;
+    }
+}
