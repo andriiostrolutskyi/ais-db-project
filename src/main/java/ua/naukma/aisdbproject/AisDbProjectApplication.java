02b2e39ed079393f010c8af57d30ca7bda3aeb57
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-06-09 01:42:41 +0300

    Working with 'PATCH' method in spring fixed

--- src/main/java/ua/naukma/aisdbproject/AisDbProjectApplication.java
+++ src/main/java/ua/naukma/aisdbproject/AisDbProjectApplication.java
@@ -2,6 +2,8 @@ package ua.naukma.aisdbproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class AisDbProjectApplication {
@@ -10,4 +12,8 @@ public class AisDbProjectApplication {
 		SpringApplication.run(AisDbProjectApplication.class, args);
 	}
 
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
